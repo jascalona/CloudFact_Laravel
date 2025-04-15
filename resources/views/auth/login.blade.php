05db10125a4c71896c0afbc1b0bfb1deb51cf2b3
root <root@destroy-latitude-5490.lan.xven.com.ve>
2025-04-15 12:52:43 -0500

    se agrego la vista del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,57 +23,57 @@
     <body>
 
 
-<section class="vh-100">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col col-xl-10">
-        <div class="card" style="border-radius: 1rem;">
-          <div class="row g-0">
-            <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="{{ asset('images/cloud.png') }}"
-                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; margin-top: 50px;" />
+    <section class="vh-100">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+            <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+                <div class="col-md-6 col-lg-5 d-none d-md-block">
+                <img src="{{ asset('images/cloud.png') }}"
+                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; margin-top: 50px;" />
+                </div>
+                <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
+
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
+
+                    <div class="d-flex align-items-center mb-3 pb-1">
+                        <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                        <span class="h1 fw-bold mb-0">Iniciar sesión</span>
+                    </div>
+
+                        <br>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                        <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" placeholder="Ingrese su Email" />
+                        <label class="form-label" for="form2Example17"></label>
+                    </div>
+
+                    <div data-mdb-input-init class="form-outline mb-4">
+                        <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" placeholder="Ingrese su Contraseña" />
+                        <label class="form-label" for="form2Example27"></label>
+                    </div>
+
+                    <div class="pt-1 mb-4">
+                        <button name="btn-log" type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-lg btn-block" value="submit">Ingresar</button>
+                    </div>
+
+                    <p class="mb-5 pb-lg-2" style="color: #393f81;">Aun no tienes cuenta? <a href="#!"
+                        style="color: #393f81;">Registrase</a></p>
+                    <a href="#!" class="small text-muted">Terms of use.</a>
+                    <a href="#!" class="small text-muted">Privacy policy</a>
+                    </form>
+
+                </div>
+                </div>
             </div>
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="card-body p-4 p-lg-5 text-black">
-
-                <form>
-
-                  <div class="d-flex align-items-center mb-3 pb-1">
-                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                    <span class="h1 fw-bold mb-0">Iniciar sesión</span>
-                  </div>
-
-                    <br>
-
-                  <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="email" id="form2Example17" class="form-control form-control-lg" placeholder="Ingrese su Email" />
-                    <label class="form-label" for="form2Example17"></label>
-                  </div>
-
-                  <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="password" id="form2Example27" class="form-control form-control-lg" placeholder="Ingrese su Contraseña" />
-                    <label class="form-label" for="form2Example27"></label>
-                  </div>
-
-                  <div class="pt-1 mb-4">
-                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-lg btn-block" type="button">Ingresar</button>
-                  </div>
-
-                  <a class="small text-muted" href="#!">Forgot password?</a>
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Don't have an account? <a href="#!"
-                      style="color: #393f81;">Register here</a></p>
-                  <a href="#!" class="small text-muted">Terms of use.</a>
-                  <a href="#!" class="small text-muted">Privacy policy</a>
-                </form>
-
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+        </div>
     </div>
-  </div>
-</section>
+    </section>
 
 
 
