62edc02b9ffca464c45a49f73bc96af305c6ce03
root <root@destroy-latitude-5490.lan.xven.com.ve>
2025-04-23 08:02:19 -0500

    se esta trabajando en la interfaz de facturacion

--- resources/views/screens/bill.blade.php
+++ resources/views/screens/bill.blade.php
@@ -182,27 +182,11 @@
                                                 aria-label="Username" aria-describedby="basic-addon1">
                                         </div>
 
-
-
                                         <div class="input-group mb-3">
-                                            <span class="input-group-text">$</span>
-                                            <input type="text" class="form-control"
-                                                aria-label="Amount (to the nearest dollar)">
-                                            <span class="input-group-text">.00</span>
+                                            <input type="text" class="form-control" placeholder="Click"
+                                                aria-label="Username" aria-describedby="basic-addon1">
                                         </div>
-
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Username"
-                                                aria-label="Username">
-                                            <span class="input-group-text">@</span>
-                                            <input type="text" class="form-control" placeholder="Server"
-                                                aria-label="Server">
-                                        </div>
-
-                                        <div class="input-group">
-                                            <span class="input-group-text">With textarea</span>
-                                            <textarea class="form-control" aria-label="With textarea"></textarea>
-                                        </div>
+                                   
                                         <!--form auto completado-->
                                     </div>
                                 </div>
